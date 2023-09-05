67877424536f5d8a27d2dcf00c3395312c051c4c
LuisCorreaDv <dev.luis.correa@gmail.com>
2023-09-05 16:25:10 -0600

    Variables y funciones para el cambio de tema

--- src/Components/UI/variables.js
+++ src/Components/UI/variables.js
@@ -1,3 +1,8 @@
 export const colorPrimario = "#41D3BE";
 export const fondoClaro = "#f1f1f1";
 export const textoFondoClaro = "gray";
+
+export const contenidoClaro = "white";
+export const fondoOscuro = "#363537";
+export const contenidoOscuro = "#5c5b5e";
+export const textoFondoOscuro = "#FAFAFA";
